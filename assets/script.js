4f36967c088638a6b3940a05f1761c876a0af970
granderojo17 <david.yennerell@gmail.com>
2020-06-10 23:53:23 -0500

    Majority of functionality is complete and now I must push the searched names to a list on the side. Most of the hard work is finally finished. :)

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
   console.log("RESULTS FOR MY-WEATHER APP: ");
   var today = moment().format("MM/DD/YYYY");
   console.log(today);
+
   if ("geolocation" in navigator) {
     console.log("Geo location Enabled");
     navigator.geolocation.getCurrentPosition((position) => {
-      // console.log( "Your current location is " + position);
+      console.log("Your current location is " + JSON.stringify(position));
       // time = moment().valueOf(navigator.geolocation.timestamp);
       // console.log(moment(time));
     });
@@ -14,104 +15,72 @@ $(document).ready(function () {
   }
 
   $("#search-button").on("click", function () {
-    let searchTerm = $("#search-term").val();
+    var searchTerm = $("#search-term").val();
     console.log("We are searching for " + searchTerm);
+    $("search-term").val("");
     searchWeather(searchTerm);
-
   });
-
+  $(".history").on("click", "li", function () {
+    searchWeather($(this).text());
+  });
+  function makeRow(text) {
+    var li = $("<li>")
+      .addClass("list-group-item list-group-item-action")
+      .text(text);
+    $(".history").append(li);
+  }
   $("#city-btn").on("click", function () {
     let searchTerm = $("#city-btn").text();
-    console.log("Gathering Weather data for" + searchTerm);
+    // console.log("Gathering Weather data for" + searchTerm);
     searchWeather(searchTerm);
-
   });
 
-
-  function fiveDayForecast(lat, lon) {
-
-    $("#five-day-forecast").empty();
-    var APIKey = "4283d387c93df34e548fe4d99a04d307";
-    var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
-
-    $.ajax({
-      url: queryURL,
-      type: "GET",
-      success: function (res) {
-        console.log("The res for fivedayForecastOneCall is:" + JSON.stringify(res));
-
-        for (var i = 0; i <= 6; i++) { //i = each request You start off with 0, run up till 4 and add once at a time.
-          var nextDay = today + 1;
-          var cityIcon = res.list[i].weather[i].icon;
-          var iconForecastURL = "http://openweathermap.org/img/wn/" + cityIcon + "@2x.png";
-          //--- FC = forecast ---
-          var cityTemperatureFC = Math.round(res.list[i].main.temp);
-          var cityHumidityFC = res.list[i].main.humidity;
-
-          let forecastCard = $("#five-day-forecast");
-          let card = $("<div>").addClass("card");
-          let forecastInfo = $("<img src=" + iconForecastURL + ">").attr("height", "45px").attr("width", "45px");
-
-          forecastCard.append(
-            $("<div>")
-              .addClass("card text-white text-center bg-success float-left")
-              .attr("id", "forecast-card")
-              .attr("style", "max-width: 12rem; margin-left: 20px")
-              .append($("<p>").html(nextDay))
-              .append(forecastInfo)
-              .append(
-                $("<p>").html("Temperature: " + cityTemperatureFC + "° F")
-              )
-              .append($("<p>").html("Humidity: " + cityHumidityFC + " %")).append("</div>")
-          );
-          $("#five-day-forecast").append(
-
-          );
-
-          $("#five-day-forecast").append(card);
-
-        }//END OF FOR LOOP
-
-
-
-      },
-    });
-  }
   function searchWeather(searchTerm) {
     //this is my current key
-    var APIKey = "4283d387c93df34e548fe4d99a04d307";
+
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       searchTerm +
-      "&appid=" +
-      APIKey +
-      "&units=imperial";
+      "&appid=4283d387c93df34e548fe4d99a04d307&units=imperial";
     // AJAX
     //on launch it should automatically call once using the location of user.
     $.ajax({
       url: queryURL,
       method: "GET",
+      dataType: "json",
       success: function (res) {
-        console.log("The res for searchWeather is:" + JSON.stringify(res));
+        // console.log("The res for searchWeather is:" + JSON.stringify(res));
         $("#today").empty();
-        var city = $("<h2>").addClass("card-title").text(res.name);
-        var temp = $("<h5>")
-          .addClass("card-text")
-          .text("Temperature: " + Math.round(res.main.temp));
-        var humidity = $("<h5>")
-          .addClass("card-text")
-          .text("Humidity: " + res.main.humidity);
-        var wind = $("<h5>")
-          .addClass("card-text")
-          .text("Wind: " + Math.round(res.wind.speed));
+
+        let title = $("<h2>")
+          .addClass("card-title")
+          .text(res.name + " (" + new Date().toLocaleDateString() + ")");
+
         var card = $("<div>").addClass("card");
         var cardBody = $("<div>").addClass("card-body");
-        getUVIndex(res.coord.lat, res.coord.lon);
 
-        cardBody.append(city, temp, humidity, wind);
+        var temp = $("<p>")
+          .addClass("card-text")
+          .text("Temperature: " + Math.round(res.main.temp) + "°F");
+        var humidity = $("<p>")
+          .addClass("card-text")
+          .text("Humidity: " + res.main.humidity + "%");
+        var wind = $("<p>")
+          .addClass("card-text")
+          .text("Wind: " + Math.round(res.wind.speed) + "MPH");
+        var img = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/w/" + res.weather[0].icon + ".png"
+        );
+        // Merge everything to the page
+        title.append(img);
+
+        cardBody.append(title, temp, humidity, wind);
         card.append(cardBody);
         $("#today").append(card);
-        fiveDayForecast(res.coord.lat, res.coord.lon);
+
+        fiveDayForecast(searchTerm);
+        getUVIndex(res.coord.lat, res.coord.lon);
       },
     });
   }
@@ -122,7 +91,7 @@ $(document).ready(function () {
       type: "GET",
       dataType: "json",
       success: function (res) {
-        console.log("The res for getUVIndex is:" + JSON.stringify(res));
+        // console.log("The res for getUVIndex is:" + JSON.stringify(res));
 
         // var card = $("<div>").addClass("card");
         // var cardBody = $("<div>").addClass("card-body");
@@ -139,3 +108,52 @@ $(document).ready(function () {
   }
 });
 // http://api.openweathermap.org/data/2.5/uvi/forecast?appid={appid}&lat={lat}&lon={lon}&cnt={cnt}
+
+function fiveDayForecast(searchTerm) {
+  var APIKey = "4283d387c93df34e548fe4d99a04d307";
+  // var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+  var queryURL2 = `http://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${APIKey}&units=imperial`;
+  $.ajax({
+    type: "GET",
+    url: queryURL2,
+
+    dataType: "json",
+    success: function (res) {
+      $("#fiveDay")
+        .html('<h4 class="mt-3">5-Day Forecast:</h4>')
+        .append('<div class="row">');
+      for (var i = 0; i < res.list.length; i++) {
+        // only look at forecasts around 3:00pm
+        if (res.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+          // create html elements for a bootstrap card
+          var col = $("<div>").addClass("col-md-2");
+          var card = $("<div>").addClass("card bg-primary text-white");
+          var body = $("<div>").addClass("card-body p-2");
+
+          var title = $("<h5>")
+            .addClass("card-title")
+            .text(new Date(res.list[i].dt_txt).toLocaleDateString());
+
+          var img = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/w/" +
+              res.list[i].weather[0].icon +
+              ".png"
+          );
+
+          var p1 = $("<p>")
+            .addClass("card-text")
+            .text("Temp: " + res.list[i].main.temp_max + " °F");
+          var p2 = $("<p>")
+            .addClass("card-text")
+            .text("Humidity: " + res.list[i].main.humidity + "%");
+
+          // merge together and put on page
+          col.append(card.append(body.append(title, img, p1, p2)));
+          $("#fiveDay .row").append(col);
+        }
+      }
+      // } //END OF FOR LOOP
+    },
+  });
+}
